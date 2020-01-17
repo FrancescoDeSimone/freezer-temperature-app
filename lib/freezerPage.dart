9389e52e86715d80736a8a0e00e8a13c6f283161
FrancescoDeSimone <f.desimone30@studenti.unisa.it>
2020-01-17 05:45:26 +0000

    Build charts

--- lib/freezerPage.dart
+++ lib/freezerPage.dart
@@ -14,21 +14,152 @@ class FreezerPage extends StatefulWidget {
 }
 
 class _FreezerPageState extends State<FreezerPage> {
-  List<Sensor> sensorsData;
-
-  Future<void> initData() async {
-    print("Get data from DB");
-    SensorDAO sd = new SensorDAO();
-    sensorsData = await sd.getDataFromSensor();
-  }
+  SensorDAO sd = new SensorDAO();
 
   @override
   Widget build(BuildContext context) {
-    initData();
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(child: Text('${sensorsData}')));
+    return MaterialApp(
+        home: DefaultTabController(
+            length: 2,
+            child: Scaffold(
+                appBar: AppBar(
+                  backgroundColor: Color(0xff1976d2),
+                  //backgroundColor: Color(0xff308e1c),
+                  bottom: TabBar(
+                    indicatorColor: Color(0xff9962D0),
+                    tabs: [
+                      Tab(
+                        text: "Humidity",
+                      ),
+                      Tab(text: "Temperature"),
+                    ],
+                  ),
+                  title: Text('Flutter Charts'),
+                ),
+                body: StreamBuilder<List<Sensor>>(
+                  stream: sd.getDataFromSensor(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<Sensor>> snapshot) {
+                    if (snapshot.hasError)
+                      return Text('Error: ${snapshot.error}');
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                        return Text('Reaching database');
+                      case ConnectionState.waiting:
+                        return Text('Awaiting Data...');
+                      case ConnectionState.active:
+                        return TabBarView(children: [
+                          Container(
+                              height: 100,
+                              child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Column(children: [
+                                    Expanded(
+                                      flex: 5,
+                                        child: new charts.TimeSeriesChart(
+                                      [
+                                        new charts.Series<Sensor, DateTime>(
+                                          id: 'Humidity',
+                                          colorFn: (_, __) => charts
+                                              .MaterialPalette
+                                              .blue
+                                              .shadeDefault,
+                                          domainFn: (Sensor s, _) => s.date,
+                                          measureFn: (Sensor s, _) =>
+                                              s.humidity,
+                                          data: snapshot.data,
+                                        ),
+                                      ],
+                                      animate: false,
+                                      defaultRenderer:
+                                          new charts.LineRendererConfig(),
+                                      customSeriesRenderers: [
+                                        new charts.PointRendererConfig(
+                                            customRendererId: 'customPoint')
+                                      ],
+                                      dateTimeFactory:
+                                          const charts.LocalDateTimeFactory(),
+                                    )),
+                                                                        Expanded(
+                                                                          flex: 5,
+child:
+                                    Container(
+                                        height: 50,
+                                        width: 100,
+                                        child: Flex(
+                                          mainAxisSize: MainAxisSize.max,
+                                          direction: Axis.horizontal,
+                                          children: [
+                                          Row(children: [
+                                            Expanded(
+                                              child:
+                                              Text(
+                                              "Average",
+                                              textAlign: TextAlign.center,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            )),
+                                                                                        Expanded(                                              child:
+
+
+                                            Text(
+                                              "Current",
+                                              textAlign: TextAlign.center,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ))
+                                          ]),
+                                          Row(children: [
+                                            Text("Average"),
+                                            Text("Current")
+                                          ])
+                                        ]))
+                          )]))),
+                          Container(
+                              child: Column(
+                            children: <Widget>[
+                              new charts.TimeSeriesChart(
+                                [
+                                  new charts.Series<Sensor, DateTime>(
+                                    id: 'Temperature',
+                                    colorFn: (_, __) =>
+                                        charts.MaterialPalette.red.shadeDefault,
+                                    domainFn: (Sensor s, _) => s.date,
+                                    measureFn: (Sensor s, _) => s.temperature,
+                                    data: snapshot.data,
+                                  ),
+                                ],
+                                animate: false,
+                                defaultRenderer:
+                                    new charts.LineRendererConfig(),
+                                customSeriesRenderers: [
+                                  new charts.PointRendererConfig(
+                                      customRendererId: 'customPoint')
+                                ],
+                                dateTimeFactory:
+                                    const charts.LocalDateTimeFactory(),
+                              ),
+                              Container(
+                                  child: Column(children: [
+                                Row(children: [
+                                  Text("Average"),
+                                  Text("Current")
+                                ]),
+                                Row(children: [
+                                  Text("Average"),
+                                  Text("Current")
+                                ])
+                              ]))
+                            ],
+                          ))
+                        ]);
+                      case ConnectionState.done:
+                        return Text('bye');
+                    }
+                    return CircularProgressIndicator();
+                  },
+                ))));
   }
 }
--- lib/sensorDAO.dart
+++ lib/sensorDAO.dart
@@ -2,25 +2,27 @@ import 'package:freezerTemperatureApp/sensor.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class SensorDAO extends DAOFactory {
-  Future<List<Sensor>> getDataFromSensor() async {
+  Stream<List<Sensor>> getDataFromSensor() async* {
     Db connection = await DAOFactory.getConnection();
     var coll = connection.collection('freezer');
-    var list = await coll.find().toList();
-    List<Sensor> dataFromSensor = new List<Sensor>();
-
-    for (var elem in list)
-      dataFromSensor.add(new Sensor(elem["humidity"], elem["temperature"],
-          elem["doorStatus"], DateTime.parse(elem["date"])));
-
-    return dataFromSensor;
+    while (true) {
+      yield await coll
+          .find()
+          .map((elem) => new Sensor(elem["humidity"], elem["temperature"],
+              elem["doorStatus"], DateTime.parse(elem["date"])))
+          .toList();
+    }
   }
 }
 
-class DAOFactory {
+abstract class DAOFactory {
+  
   static Future<Db> getConnection() async {
     Db db = new Db(
         "mongodb://freezer-db:kfFOvwxC7XcsIBvXFX9OKF72Pk9GVHOPFLZcgBE8dQcsP4yPaoJXBRSt2Tbq8w0JLRzhuK0OIoLg4oOOYTrLwg==@freezer-db.documents.azure.com:10255/freezer-db");
     await db.open(secure: true);
     return db;
   }
+
+  Stream<List<Sensor>> getDataFromSensor();
 }
